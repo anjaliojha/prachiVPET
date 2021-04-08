4e2fb9519d3ce0668f5c1c36638f201fedb71c6c
Prachi106-star <75433997+Prachi106-star@users.noreply.github.com>
2021-04-08 10:52:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,68 @@
+//Create variables here
+var dog , happyDog , database , foodS , foodStock ;
+
+
+function preload()
+{
+  //load images here
+  dogImg =  loadImage("dogImg.png");
+  happyDogImg = loadImage("dogImg1.png");
+  //eatingDog = loadImage("eating shadow.jpg");
+  //eatingDog.scale = 0.5 ;
+}
+
+function setup() {
+  database = firebase.database();
+  foodStock = database.ref("food");
+  foodStock.on("value",readstock);
+  createCanvas(800,600);
+  dog =  createSprite(150,400);
+  dog.addImage(dogImg);
+
+  dog.scale = 0.3
+  
+}
+
+
+function draw() {  
+background(46,139,87);
+
+textSize(20);
+fill("black");
+text("Hey Hi Everyone I am Shadow !! ",350,50);
+
+textSize(20);
+fill("black");
+text("Press the UP_ARROW to play with shadow ",350,390);
+
+//fill("black");
+//text("Press the DOWN_ARROW to feed shadow ",300,350);
+
+drawSprites();  
+  //add styles here
+  if (keyWentDown(UP_ARROW)){
+    writestock(foodS);
+    dog.addImage(happyDogImg);
+  // fill("black");
+    //text("It is playing time !!",350,50);
+   // dog.scale = 0.2 ;
+  }
+
+ /* if (keyWentDown(DOWN_ARROW)){
+    writestock(foodS);
+    dog.addImage(eatingDog);
+  }*/
+
+}
+
+function readstock(data){
+  foodS = data.val();
+}
+
+function writestock(x){
+  database.ref("/").update({
+    food:x 
+  })
+}
+
+
